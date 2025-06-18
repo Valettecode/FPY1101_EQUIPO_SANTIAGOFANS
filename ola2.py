59ea30ea73ea63aa31028ed6570cb17f9c2c014d
Diego Gonzalez <dij.gonzalez@duocuc.cl>
2025-06-18 09:16:43 -0400

    Update ola2.py

--- ola2.py
+++ ola2.py
@@ -1,1 +1,7 @@
+print("Santiago eres entero weon")
 
+s=int(input("Es santiago weon?(1-si)(2-no))
+if s=1
+print ("JAJAJAJ SANTIAGO ERES DEMASIADO INVESIL)
+       else:
+print("de todas formas santiago es tonto, gracias)
