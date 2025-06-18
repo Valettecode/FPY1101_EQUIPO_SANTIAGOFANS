623ff21ff2f487aad45eef365c24ba005807669a
Raimundo Navarro <raim.navarro@duocuc.cl>
2025-06-18 09:52:04 -0400

    Delete Raimundocarpeta directory
    No sirve para nada

--- Raimundocarpeta/test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-bla = input("Ingresa tu edad: ")
-
-print("Borrando datos de tu pç...")
-time.sleep(2)
-print("Datos borrados con exito")
-
-print("Santiago estuvo aqui, te odio raimundo")
