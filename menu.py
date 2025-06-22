eab3f245a53d71e67e92834ac2cb9c786421d2f9
Diego Gonzalez <dij.gonzalez@duocuc.cl>
2025-06-22 18:55:04 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ while True:
  print("1. Función de integrante 1")
  print("2. Función de integrante 2")
  print("3. Función de integrante 3")
+ print("4.Funcion de integrante 4")
  print("0. Salir")
  op = input("Seleccione opción: ")
  if op == "0":
@@ -18,5 +19,6 @@ while True:
  pass # Aquí se llamará a la función del integrante 2
  elif op == "3":
  pass # Aquí se llamará a la función del integrante 3
+elif op== "4":
  else:
  print(" Opción inválida.")
