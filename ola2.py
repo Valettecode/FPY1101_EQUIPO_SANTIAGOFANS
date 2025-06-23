a5edc84e28e05cee1f8d4a9c2c0de09f2c52661e
Diego Gonzalez <dij.gonzalez@duocuc.cl>
2025-06-22 20:00:09 -0400

    Update ola2.py

--- ola2.py
+++ ola2.py
@@ -1,14 +1,1 @@
-while True
- print("Santiago eres entero weon")
 
-s=int(input("Es santiago weon?(1-si)(2-no)")
-if s=1
-print ("JAJAJAJ SANTIAGO ERES DEMASIADO INVESIL")
-       else:
-print("de todas formas santiago es tonto, gracias")
-print("aaa")
-
-print("madre mia chaval")
-
-Github con friendly fire porque eres asi
-SANTIAGO ESTUVO AQUI
